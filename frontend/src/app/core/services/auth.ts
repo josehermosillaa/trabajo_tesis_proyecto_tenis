9200691350820672fdee35f92f06b0bb023af658
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-07-09 18:42:24 -0400

    feat(auth): initialize AuthService structure

---
+++ frontend/src/app/core/services/auth.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+
+import { LoginRequest } from '../models/login-request';
+import { TokenResponse } from '../models/token-response';
+import { TokenService } from './token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly http = inject(HttpClient);
+  private readonly tokenService = inject(TokenService);
+
+  private readonly apiUrl = `${environment.apiUrl}/token`;
+
+}
