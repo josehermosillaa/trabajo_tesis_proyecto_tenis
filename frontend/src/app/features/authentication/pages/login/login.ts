83f9b15c840fd4c54e891209763396982d47bc67
jose.hermosillaa <jose.hermosillaa@usach.cl>
2026-07-10 12:50:45 -0400

    feat(auth): integrate login with TokenService

--- frontend/src/app/features/authentication/pages/login/login.ts
+++ frontend/src/app/features/authentication/pages/login/login.ts
@@ -1,7 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
+import { Router } from '@angular/router';
 
+import { AuthService } from '../../../../core/services/auth';
+import { TokenService } from '../../../../core/services/token';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
@@ -12,8 +15,41 @@ export class LoginComponent {
 
   private readonly fb = inject(FormBuilder);
 
+  private readonly authService = inject(AuthService);
+  private readonly tokenService = inject(TokenService);
+  private readonly router = inject(Router);
+
   readonly loginForm = this.fb.nonNullable.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
   });
+
+  onSubmit(): void {
+  console.log('Entró al onSubmit');
+  if (this.loginForm.invalid) {
+    this.loginForm.markAllAsTouched();
+    return;
+  }
+  const credentials = this.loginForm.getRawValue();
+
+  this.authService.login(credentials).subscribe({
+  next: (response) => {
+
+    this.tokenService.saveTokens(
+      response.access,
+      response.refresh
+    );
+
+    console.log('Tokens guardados correctamente');
+
+  },
+  error: (error) => {
+    console.error(error);
+  }
+
+});
+
+}
+
+
 }
